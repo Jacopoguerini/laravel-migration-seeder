9d72779fa6ddee90bbffaf350b32b1f9708ae588
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-15 16:02:53 +0200

    Inserito file dati in config e creato seeder

---
+++ config/packages.php
@@ -0,0 +1,33 @@
+<?php
+$data = [
+    [
+        "package_name"=> "Viaggio rilassante nel Sud di Zanzibar",
+        "city" => "Jambiani",
+        "country" => "Zanzibar, Tanzania",
+        "type" => "Relax",
+        "structure_name" => "Bahari Pizza",
+        "structure_type" => "Hotel",
+        "rating" => 4.3,
+        "price_night_person" => 30,
+        "departure" => "2021-08-10",
+        "return" => "2021-08-29",
+        "free_cancellation" => 1,
+        "description" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro iste fuga, quam qui facere dolores officiis? Voluptatem, nisi. Similique cupiditate tempore exercitationem, sapiente, repellat tenetur non cumque, soluta id neque incidunt beatae inventore cum minima facilis odio velit quos voluptate vitae? Quasi laudantium aliquam fugiat dolor iure! Velit tempore temporibus sunt neque cum, dolor assumenda, atque eius, consectetur odio eligendi magni recusandae maiores ut consequatur saepe facere! Dolor, ut facere placeat dicta nisi, quia consequatur iusto sed veniam eaque sapiente iure velit debitis. Molestiae saepe, ipsum officiis nemo aut enim? Aspernatur dicta natus voluptas pariatur? Consequuntur sed quas veniam! Quia!"
+    ],
+    [
+        "package_name"=> "Avventura nella ridente Mordor",
+        "city" => "Barad-dûr",
+        "country" => "Mordor",
+        "type" => "Avventura",
+        "structure_name" => "Minas Morgul",
+        "structure_type" => "Fortezza",
+        "rating" => 5,
+        "price_night_person" => 50,
+        "departure" => "2021-09-01",
+        "return" => "2021-09-16",
+        "free_cancellation" => 0,
+        "description" => "Minas Morgul was once a fortress of Gondor, called Minas Ithil, the Tower of the Moon. As the easternmost fortification in the kingdom of Gondor, Minas Ithil safeguarded the eastern borders of the Kingdom of Gondor and protected the capital Osgiliath from the forces of Mordor during the early part of the Third Age. As Gondor's armies weakened, it was then taken by the forces of Mordor, and used as a base to attack Gondor and in the process, decayed into the dark fortress and was renamed as a result."
+    ]
+];
+
+return $data;
