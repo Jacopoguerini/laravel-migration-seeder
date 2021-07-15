747e212b3956232694ddaa5f3c0fdb04554816f5
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-15 16:23:49 +0200

    Tentativo di invio file al database

--- database/seeds/PackagesTableSeeder.php
+++ database/seeds/PackagesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Package;
 
 class PackagesTableSeeder extends Seeder
 {
@@ -11,6 +12,27 @@ class PackagesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $arrayPackage = config('package');
+
+        foreach ($arrayPackage as $item) {
+            $package = new Package();
+
+            $package->{'package_name'} = $item["package_name"];
+            $package->city = $item["city"];
+            $package->country = $item["country"];
+            $package->type = $item["type"];
+            $package->{'structure_name'} = $item["structure_name"];
+            $package->{'structure_type'} = $item["structure_type"];
+            $package->rating = $item["rating"];
+            $package->{'price_night_person'} = $item["price_night_person"];
+            $package->departure = $item["departure"];
+            $package->return = $item["return"];
+            $package->{'free_cancellation'} = $item["free_cancellation"];
+            $package->description = $item["description"];
+
+            $package->save();
+        }
+
+
     }
 }
